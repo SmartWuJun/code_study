7d76889812b9a594b26ae80a0bbc4b5fc9430355
wujun <wujun@ft.com>
2022-07-19 08:43:19 +0800

    测试

--- vue/watcher.js
+++ vue/watcher.js
@@ -1,32 +1,36 @@
-
-
+/*
+ * @Author: WUJUN
+ * @Date: 2022-07-19 08:18:38
+ * @Description:
+ */
 let id = 0;
 class Watcher {
   id = 0;
   constructor(vm, expOrFn, cb) {
-    this.id = id++;
+    id++;
+    this.id = id;
     this.vm = vm;
-    if (typeof expOrFn === 'function') {
-      this.getter = expOrFn
+    if (typeof expOrFn === "function") {
+      this.getter = expOrFn;
     } else {
-      this.getter = function (obj) { return obj[expOrFn] }
+      this.getter = function (obj) {
+        return obj[expOrFn];
+      };
     }
     this.get();
     this.cb = cb;
   }
-  addDep () {
-
-  }
-  get () {
+  addDep() {}
+  get() {
     Dep.target = this;
 
-    this.value = this.getter(this.vm, this.vm._data);
+    this.value = this.getter.call(this.vm, this.vm._data);
 
     Dep.target = null;
   }
-  update () {
+  update() {
     this.get();
-    this.cb && this.cb()
-    console.log('watcher update')
+    this.cb && this.cb();
+    console.log("watcher update");
   }
 }
